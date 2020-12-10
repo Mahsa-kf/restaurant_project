dd54439f99446c03a55e47fa147b013990a1272e
gmhumber <gordian.mok@gmail.com>
2020-12-10 16:42:06 -0500

    Adjust fadeIn duration.

--- js/about.js
+++ js/about.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $('.fadeEffect').fadeOut(0);
 
     $('.about-section-container').hover(function(){
-        $(this).find('p').fadeIn(2500);
+        $(this).find('p').fadeIn(2250);
         $(this).find('img').fadeIn(2000);
         });
 
