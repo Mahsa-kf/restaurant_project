9d2c53cdf44664ca82ad3a502d75034fa62ff8d8
Mahsa Karim Fard <Mahsa.kf@yahoo.com>
2020-12-11 13:12:43 -0500

    Updated sitemap

--- js/site-map.js
+++ js/site-map.js
@@ -1,30 +1,29 @@
 
 window.onload = function () {
 
-    // Get all collapse-btn element and put them in an array
-    var collapseBtn = document.getElementsByClassName("collapse-btn");
+    
+    // Get all collapseTitle element and put them in an array
+    var collapseTitle = document.getElementsByClassName("collapsible-title");
 
     // Create listener for all collapse buttons
-    for (var i = 0; i < collapseBtn.length; i++) {
-        collapseBtn[i].onclick = onCollapseClicked;
+    for (var i = 0; i < collapseTitle.length; i++) {
+        collapseTitle[i].onclick = onTitleClicked;
     }
 
-    function onCollapseClicked() {
+    function onTitleClicked() {
         // Get the content of clicked item
         var collapsedContent = this.nextElementSibling;
 
         if (collapsedContent.style.display == "none") {
             // Show collapse content
             collapsedContent.style.display = "block";
-            // Change the collapse icon to "-"
-            this.innerText = '-';
-            this.style.fontSize = '30px';
+            this.children[1].style.transform = "none";
         } else {
             // Hide collapse content
             collapsedContent.style.display = "none";
-            // Change the collapse icon to "+""
-            this.innerText = '+';
-            this.style.fontSize = '22px';
+            //Rotate the collapse icon 180 degrees to make it expand the icon
+            this.children[1].style.transform = "rotateX(180deg)";
+
         }
     }
 
